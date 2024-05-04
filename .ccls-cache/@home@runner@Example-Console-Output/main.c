a003b34ad78a92dd02301367a69e1efe54af054f
Rai <contact@raibahadursingh.com>
2024-05-04 11:55:01 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Example-Console-Output/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+  // "Good morning" ਪ੍ਰਿੰਟ ਕਰੋ, ਨਵੀਂ ਲਾਈਨ 'ਤੇ ਜਾਓ, ਫਿਰ "Ms Smith." ਪ੍ਰਿੰਟ ਕਰੋ ਅਤੇ ਫਿਰ ਇੱਕ
+  // ਹੋਰ ਨਵੀਂ ਲਾਈਨ 'ਤੇ ਜਾਓ
+  printf("Good morning\nMs Smith.\n");
+  int age = 25; // age ਵੇਰੀਏਬਲ ਨੂੰ 25 ਦਾ ਮੁੱਲ ਦਿਓ
+  printf("Age is %d\n", age); // "Age is " ਪ੍ਰਿੰਟ ਕਰੋ ਅਤੇ ਫਿਰ age ਦਾ ਮੁੱਲ ਪ੍ਰਿੰਟ ਕਰੋ
+
+  return 0;
+}
